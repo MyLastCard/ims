71a11b51afd6a69c71aaefc2f6b57f21a0f46585
MyLastCard <vonwao@hotmail.com>
2013-03-19 00:57:11 -0400

    moved some stuff to flatten directory structure

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var app = express();
 
 app.configure(function(){
 	app.set('port', 8080);
-	app.set('views', __dirname + '/app/server/views');
+	app.set('views', __dirname + '/views');
 	app.set('view engine', 'jade');
 	app.locals.pretty = true;
 //	app.use(express.favicon());
@@ -20,15 +20,15 @@ app.configure(function(){
 	app.use(express.cookieParser());
 	app.use(express.session({ secret: 'super-duper-secret-secret' }));
 	app.use(express.methodOverride());
-	app.use(require('stylus').middleware({ src: __dirname + '/app/public' }));
-	app.use(express.static(__dirname + '/app/public'));
+	app.use(require('stylus').middleware({ src: __dirname + '/public' }));
+	app.use(express.static(__dirname + '/public'));
 });
 
 app.configure('development', function(){
 	app.use(express.errorHandler());
 });
 
-require('./app/server/router')(app);
+require('./router')(app);
 
 http.createServer(app).listen(app.get('port'), function(){
 	console.log("Express server listening on port " + app.get('port'));
